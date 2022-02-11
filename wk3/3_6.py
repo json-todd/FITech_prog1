77db113069851a66a0af54d2b7332c18d6f6fec8
Tri Phung <cadbane1998@gmail.com>
2022-02-11 18:02:56 +0200

    wk3 exercise 6

---
+++ wk3/3_6.py
@@ -0,0 +1,40 @@
+"""
+Author: Tri Phung
+StudentID: tuni.fi:K441912
+COMP.CS.100 Programming 1
+Template for task: box printing
+"""
+
+def print_box(width:int, height:int, mark:str):
+    """Draw a box with width x height in the mark
+
+    Args:
+        width (int): the width of the box
+        height (int): the height of the box
+        mark (char): the mark displayed
+    """
+    box = ""
+    
+    width = int(width); height = int(height)
+    for row in range(height):
+        box += width * mark
+        if row != height-1:
+           box += "\n"
+        else:
+            continue
+        
+    print(box)
+    
+
+
+def main():
+    width = input("Enter the width of a frame: ")
+    height = input("Enter the height of a frame: ")
+    mark = input("Enter a print mark: ")
+
+    print()
+    print_box(width, height, mark)
+
+
+if __name__ == "__main__":
+    main()
