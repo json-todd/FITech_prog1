36c6d915a79969305e765e0f4d1aa688286db1d1
Tri Phung <cadbane1998@gmail.com>
2022-05-23 08:46:32 +0300

    pt12_ex5: complete game

--- pt12/12_5.py
+++ pt12/12_5.py
@@ -1,39 +1,85 @@
+"""
+COMP.CS.100
+Chapter 12 Section 5: Battleship
+Game description: On a 10x10 game board, the player attempts to sink the computer's fleet.
+Player declare coordinates designating the tiles on gamebard, make educated guess, and deduce tiles containing enemy's ships.
+Player won when all ships are sunk.
+
+Author: Tri Phung
+Email: tri.phung@tuni.fi
+Student ID: tuni.fi:K441912
+
+"""
+
 class Ship():
-    battle_station = []
+    __battle_station = []
     def __init__(self, name: str, location: list):
-        self.name = name
-        self.locations = location
-        self.damage = []
+        self.__name = name
+        self.__locations = location
+        self.__damage = []
       
-        for location in self.locations:
-          Ship.battle_station.append(location)
+        for location in self.__locations:
+          Ship.__battle_station.append(location)
 
     def has_overlap(self) -> bool:
-        return len(set(Ship.battle_station)) != len(Ship.battle_station)
+        """Return true if Ships have overlapping coordinates"""
+        return len(set(Ship.__battle_station)) != len(Ship.__battle_station)
 
     def get_name(self) -> str:
-        return self.name[0].upper()
+        """Return capitalized initial letter of ship name"""
+        return self.__name[0].upper()
 
     def get_full_name(self) -> str:
-        return self.name
+        """Return ship name"""
+        return self.__name
     
     def get_locations(self) -> list:
-        return self.locations
+        """Return all ship's locations"""
+        return self.__locations
   
     def take_damage(self, location) -> None:
-        self.damage.append(location)
+        """Register the location where the shit gets hit
+        Hit location is then stored into an array for later implementation
+        """
+        self.__damage.append(location)
 
     def has_full_damage(self) -> bool:
-        return len(self.damage) == len(self.locations)
+        """When ship has all of its location hits, it has taken full damaged.
+        Return true when ship is full damaged.
+        """
+        return len(self.__damage) == len(self.__locations)
 
+
+class Board():
+    ALPHABET: list = ['A','B','C','D','E','F','G','H','I','J']
+    BOARD_SIDE: int = 10
+    SHIP_LIST: list = []
+    
+    def __init__(self):
+        pass
+        
+    @classmethod
+    def add_ship(self, ship: Ship) -> None:
+        """Add Ship to a class attribute"""
+        Board.SHIP_LIST.append(ship)
+    
+    @classmethod
+    def get_ships(self) -> list:
+        """Return all ships on the board"""
+        return Board.SHIP_LIST
+    
 
 def read_file() -> bool:
+    """Read the input file
+
+    Returns:
+        bool: exit status of this function. True if success, False if error or invalid.
+    """
     file = input('Enter file name: ')
-    # if len(file) == 0: file = 'board.txt'
+    if len(file) == 0: file = 'board.txt'
 
     try:
         file_handle = open(file,'r')
-        global SHIP_LIST
         
         for line in file_handle:
             line = line.rstrip()
@@ -46,8 +92,9 @@ def read_file() -> bool:
                     print('Error in ship coordinates!')
                     return False
             
+            
             Ship_object = Ship(name, coordinate)
-            SHIP_LIST.append( Ship_object )
+            Board.add_ship( Ship_object )
             
             if Ship_object.has_overlap():
                 print('There are overlapping ships in the input file!')
@@ -56,25 +103,38 @@ def read_file() -> bool:
         return True
             
     except:
-        print('File cannot be read!')
+        print('File can not be read!')
         return False
 
         
 def get_valid_coordinate(xy_coord: str) -> tuple or None:
-    global ALPHABET
+    """Check and return the correct format of coordinate.
+
+    Args:
+        xy_coord (str): coordinate in format of ALPHABET-NUMBER, e.g. 'A1'
+
+    Returns:
+        tuple or None: coordinate of x and y in numerical type when valid. None otherwise
+    """
+    ALPHABET = Board.ALPHABET
     try:
         assert len(xy_coord) == 2
         (x_coord_txt, y_coord_txt) = [coord for coord in xy_coord]
         x_coord = ALPHABET.index(x_coord_txt.upper())
         y_coord = int(y_coord_txt)
-        assert 0 <= y_coord <= 9
         return (x_coord, y_coord)
     except:
         return None
 
         
 def print_board(current_board: list) -> None:
-    global ALPHABET, BOARD_SIDE
+    """Print the board with style
+
+    Args:
+        current_board (list): Game board
+    """
+    ALPHABET = Board.ALPHABET
+    BOARD_SIDE = Board.BOARD_SIDE
 
     print()
     print(' '*2 + ' '.join(ALPHABET) + ' '*2)
@@ -89,13 +149,25 @@ def print_board(current_board: list) -> None:
 
 
 def shoot_coordiate(current_board: list, x_coord: int, y_coord: int) -> list:
+    """Shoot the location at `x_coord` and `y_coord`.
+    Mark the tile appropriately.
+
+    Args:
+        current_board (list): game board
+        x_coord (int): x coordinate, as an index for list accessing
+        y_coord (int): y coordinate, as an index for list accessing
+
+    Returns:
+        update_board(list): mark at the shot locoation and update the board.
+    """
     update_board: list = current_board[:]
     marking: str = '*'
-    global SHIP_LIST
-
+    ship_list = Board.get_ships()
+    ALPHABET = Board.ALPHABET
     xy_coord: str = ALPHABET[x_coord] + str(y_coord);
     
-    for ship in SHIP_LIST:
+    for ship in ship_list:
+        ship: Ship
         ship_locations: list = ship.get_locations()
         if xy_coord in ship_locations:
             marking = 'X'
@@ -103,7 +175,7 @@ def shoot_coordiate(current_board: list, x_coord: int, y_coord: int) -> list:
             if ship.has_full_damage():
                 sunk_ship = ship
                 marking = ship.get_name()
-                print(f'You sunk a {ship.get_full_name()}!')
+                print(f'You sank a {ship.get_full_name()}!')
 
     if marking == '*' or marking == 'X':
         update_board[y_coord][x_coord] = marking
@@ -114,10 +186,19 @@ def shoot_coordiate(current_board: list, x_coord: int, y_coord: int) -> list:
 
 
 def reveal_ship(current_board: list, sunk_ship: Ship) -> list:
+    """Reveal ship name at all occupying tiles when it has full damage.
+    Tiles of ship location display ship's capitalized initial letter.
+
+    Args:
+        current_board (list): game board
+        sunk_ship (Ship): ship object that has taken all of locations shot
+
+    Returns:
+        update_board(list): update game board where tiles reveal ship's capitalized initial letter.
+    """
     update_board: list = current_board[:]
     ship_name: str = sunk_ship.get_name()
     ship_locations: list = sunk_ship.get_locations()
-    global ALPHABET
     
     for location in ship_locations:
         (x_coord, y_coord) = get_valid_coordinate(location)
@@ -127,28 +208,28 @@ def reveal_ship(current_board: list, sunk_ship: Ship) -> list:
 
 
 def has_sunk_all_ship() -> bool:
-    global SHIP_LIST
-    ship_count = len(SHIP_LIST)
-    sunk_ship_count = sum([1 for ship in SHIP_LIST if ship.has_full_damage()])
+    """Return true if the number of ship sunk equals to the number of all ships"""
+    ship_list = Board.get_ships()
+    ship_count = len(ship_list)
+    sunk_ship_count = sum([1 for ship in ship_list if ship.has_full_damage()])
     
     return sunk_ship_count == ship_count
 
-ALPHABET: list = ['A','B','C','D','E','F','G','H','I','J']
-BOARD_SIDE: int = 10
-SHIP_LIST: list = []
 
 def main():
-
     board = [[' ']*10 for _ in range(10)]
     shot_location = []
 
+    # Read input file. If unsuccessful, program terminated
     read_file_exit_status = read_file()
     if not read_file_exit_status:
         return False
     
+    # Main program loop
     while True:
         print_board(board)
 
+        # Check winning condition. If true, loop ends
         if has_sunk_all_ship():
             print('Congratulations! You sank all enemy ships.')
             break
@@ -159,18 +240,22 @@ def main():
             print('Aborting game!')
             break  
 
+        # Validate user's input to have right formate
         try:
             (x_coord, y_coord) = get_valid_coordinate(user_input)
         except:
             print('Invalid command!')
             continue
 
-        if user_input in shot_location:
+        # Validate location to be a new one
+        if user_input.upper() in shot_location:
             print('Location has already been shot at!')
             continue
         
+        # Proceed shooting at the coordinate
+        # Add the shot coordinate in a historical ledger.
         board = shoot_coordiate(board, x_coord, y_coord)
-        shot_location.append(user_input)
+        shot_location.append(user_input.upper())
 
     return True
 
